64cca8600ed9fb2f03dae02ba8f447cecc1c144f
userName <>
2023-01-12 09:53:28 +0500

    start project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+	return <>
+		<Head>
+			<title>MyTop - best top</title>
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+		<Component {...pageProps} />;
+	
+	</>;
 }
+
+export default MyApp;
