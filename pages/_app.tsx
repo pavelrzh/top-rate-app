e92e0d6cc5f7c27ddb23b44510e06f9081482cfb
userName <>
2023-03-09 16:47:23 +0500

    add <meta>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,15 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
 	return <>
 		<Head>
 			<title>MyTop - best top</title>
 			<link rel="icon" href="/favicon.ico" />
+			<meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+			<meta property="og:locale" content='ru_RU' />
 		</Head>
 		<Component {...pageProps} />
-	
 	</>;
 }
 
