a8f1f5f043fc3c943023b05741dd7eb38a449e2f
userName <>
2023-03-10 12:54:48 +0500

    add husky

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
 import '../styles/globals.css';
+import React from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import ym from 'react-yandex-metrika';
 import { YMInitializer } from 'react-yandex-metrika';
+import { Router } from 'next/router';
+
+Router.events.on('routeChangeComplete', (url: string) => {
+	if (typeof window !== 'undefined') {
+		ym('hit', url);
+	}
+});
 
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
-	router.events.on('routeChangeComplete', (url: string) => {
-		if (typeof window !== 'undefined') {
-			ym('hit', url);
-		}
-	});
 
 	return <>
 		<Head>
