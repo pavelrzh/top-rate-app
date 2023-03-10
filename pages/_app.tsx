df79a1cbcbf66c0962466fee26c0100b94a897a0
userName <>
2023-03-10 09:50:31 +0500

    add yandex metrika

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import ym from 'react-yandex-metrika';
+import { YMInitializer } from 'react-yandex-metrika';
 
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
+	router.events.on('routeChangeComplete', (url: string) => {
+		if (typeof window !== 'undefined') {
+			ym('hit', url);
+		}
+	});
+
 	return <>
 		<Head>
 			<title>MyTop - best top</title>
@@ -11,6 +19,11 @@ function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
 			<meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
 			<meta property="og:locale" content='ru_RU' />
 		</Head>
+		<YMInitializer 
+			accounts={[]}
+			options={{ webvisor: false, defer: true }}
+			version='2'
+		/>
 		<Component {...pageProps} />
 	</>;
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ function Document() {
 			<Head>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link rel="preconnect" href="https://mc.yandex.ru" />
 				<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
 			</Head>
 			<body>
